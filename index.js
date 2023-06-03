b1d6be08103dcc454463fbbc18dd0fafa450e6b9
kobe-koto <91669733+kobe-koto@users.noreply.github.com>
2023-06-03 15:22:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,22 @@
-/* Config Start */
+/* Base Config Start */
 const 
       Endpoint    = "link.storjshare.io",
       AccessKeyID = "jvxlx2yv5uctti2kojl6zeniwd4a",
-      BucketID    = "koto-cc-bucket-us",
-      UseOrigin   = false;
-/* Config End */
+      BucketID    = "koto-cc-bucket-us";
+/* Base Config End */
 
-
-
+/* Ext Config Start */
+const 
+    ExtFlags = {
+        UseStaticFilesFromOrigin: false,
+        StaticCDNLink: "https://static.koto.cc/StorjStatic",
+        UseTweaks: true,
+        HideNavBar: true
+        // SortTheContents: true (todo)
+    }
+/* Ext Config End */
 
 const ShareURL = `https://${Endpoint}/s/${AccessKeyID}/${BucketID}`;
-
-const InjectCSS = `
-    * {
-        transition: all 0.3s ease;
-    }
-    body {
-        min-height: 100vh;
-        background: #F9FAFC;
-    }
-    footer {
-        display: none;
-    }
-
-    .navbar .d-done {
-        display: none;
-    }
-    .directory-link .row {
-        border-radius: 8px !important;
-    }
-
-    .container-fluid .row .sidebar {
-        display: none !important;
-    }
-
-    .breadcrumbs > span.list-desc {
-        opacity: 0.65;
-    }
-    .container-fluid > p {
-        display: none;
-    }
-
-    .container-fluid .row .content .row.justify-content-center > div * {
-        margin: 0 !important;
-    }
-
-
-    @media (min-width: 768px) {
-        .col-md-5, .col-md-7, .col-lg-8 {
-            -ms-flex: 1 !important;
-            flex: 1 !important;
-            max-width: none !important;
-        }
-
-
-        .container-fluid .row .content {
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-            height: 100vh;
-        } 
-        .container-fluid .row .content .row.justify-content-center > div {
-            padding-bottom: 0 !important;
-        }
-        .container-fluid .row .content .row.justify-content-center > div * {
-            height: 95vh;
-        }
-
-        .container-fluid .row .content .row.pt-4.pb-2 {
-            display: block;
-        }
-
-
-        .container-fluid .row .content .row.pt-4.pb-2 > div {
-            text-align: center !important;
-        }
-    }
-`
-.replace(/(\040\040|\n)/gi, "")
-.replace(/\040{/gi, "{")
-.replace(/\040:/gi, ":")
-//.replace(/\040/gi, "")
-.replace(/;}/gi, "}")
 
 addEventListener("fetch", event => {
     event.respondWith(main(event.request));
@@ -105,15 +40,14 @@ async function main(request) {
 
 
     //console.log(NewRequestHeaders)
+    let TargetURL = `${ShareURL}${CurrentURL.pathname}${CurrentURL.search}`;
 
-    let OriginalResponse = await fetch(`${ShareURL}${CurrentURL.pathname}${CurrentURL.search}`, {
+    console.log(TargetURL)
+
+    let OriginalResponse = await fetch(TargetURL, {
         method: request.method,
         headers: NewRequestHeaders
     })
-
-    if (NewRequestHeaders.get("Upgrade") && NewRequestHeaders.get("Upgrade").toLowerCase() === "websocket") {
-        return OriginalResponse;
-    }
 
     const { status } = OriginalResponse;
 
@@ -128,12 +62,44 @@ async function main(request) {
     if ((NewResponseHeaders.get("content-type") || "").includes("text/html")) {
         let ReplacedText = await OriginalResponse.text();
         
-        ReplacedText = ReplacedText.replace(/<\/head>/, "<style>" + InjectCSS + "</style></head>");
+        
 
-        ReplacedText = ReplacedText.replaceAll(
-            `<a href="/s/${AccessKeyID}/${BucketID}/">${BucketID}</a>`, 
-            `<span class="list-desc">List of</span>&nbsp;<a href="/">Root</a>`
-        )
+        if (!ExtFlags.UseStaticFilesFromOrigin) {
+            ReplacedText = ReplacedText.replaceAll(
+                `https://link.storjshare.io/static/`, 
+                `${ExtFlags.StaticCDNLink}/`
+            )
+            ReplacedText = ReplacedText.replaceAll(
+                `https://unpkg.com/leaflet@1.7.1/dist/`, 
+                `${ExtFlags.StaticCDNLink}/leaflet@1.7.1/`
+            )
+            ReplacedText = ReplacedText.replaceAll(
+                `https://fonts.googleapis.com/css?family=Inter:300,700`, 
+                `${ExtFlags.StaticCDNLink}/GoogleFont-Inter/main.css`
+            )
+        }
+
+        if (ExtFlags.UseTweaks) {
+            ReplacedText = ReplacedText.replace(
+                "</head>",
+                `<link rel="stylesheet" href="${ExtFlags.StaticCDNLink}/tweaks.css">`
+                + "</head>"
+            );
+
+            ReplacedText = ReplacedText.replaceAll(
+                `<a href="/s/${AccessKeyID}/${BucketID}/">${BucketID}</a>`, 
+                `<span class="list-desc">List of</span>&nbsp;<a href="/">Root</a>`
+            )
+        }
+
+        if (ExtFlags.HideNavBar) {
+            ReplacedText = ReplacedText.replace(
+                "</head>",
+                `<link rel="stylesheet" href="${ExtFlags.StaticCDNLink}/NavHider.css">`
+                + "</head>"
+            );
+        }
+
         ReplacedText = ReplacedText.replaceAll(
             `/s/${AccessKeyID}/${BucketID}/`, 
             `/`
